d3ddbf4dcf752a55d7ba2a7673bb8554eeecbfba
LAHOOAZ <Lacey.Anderson@gmail.com>
2014-11-19 00:47:21 -0500

    My Programming Assignment - V1
    My first cut at programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow you to caclulate
+## and store the value of an inverse matrix
+## if a matrix has already been inverted
+## the function returns the stored value
+## rather than recalculating.
+##
+## Usage Example:
+## exMatrix <- matrix(1:4, 2:5)
+## matrixObj1 <- makeCacheMatrix(exMatrix)
+## cacheSolve(matrixObj1)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCachMatrix create an object
+## that can be used to get & set a matrix object
+makeCacheMatrix <- function(myMatrix = matrix()) {
+  
+  inverseMatrix <- NULL
+  
+  set <- function(givenMatrix){
+    myMatrix <<- givenMatrix
+    inverseMatrix <<- NULL
+  }
+     
+  #returns the value of the matrix passed in
+  get <- function() myMatrix 
+  
+  #stores the value of the inverse Matrix
+  setInverse <- function(givenMatrix) 
+    { inverseMatrix <<- givenMatrix }
+   
+  #returns the value of the inverse matrix
+  getInverse <- function() inverseMatrix
+  
+  list(set = set, get = get, setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
 
-## Write a short comment describing this function
+## Use the make cache matrix function to store
+## an inverse matrix once it is calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  #access the object created by makeCacheMatrix
+  inverseMatrix <- x$getInverse()
+  
+  #if it is not null return the stored value
+  if(!is.null(inverseMatrix)){
+    message("getting cached data")
+    return(inverseMatrix)
+  }
+  
+  #else we are going to calculate and store
+  matrix <- x$get()
+  
+  #calculate the inverse
+  inverseMatrix <- solve(myMatrix, ...)
+  
+  #set/store the inverse
+  x$setInverse(inverseMatrix)
+  
+  #return the value of the inverse matrix
+  inverseMatrix 
+  }
